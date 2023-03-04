8c952a50dcafd22e971b5d4f959c4880a308c414
James <jamesmcging@gmail.com>
2023-03-04 22:01:47 +0000

    initial commit

--- models/index.js
+++ models/index.js
@@ -1,19 +1,11 @@
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
 
-const config = require(__dirname + '/../config/db.config.js')[env];
+const Sequelize = require('sequelize');
+let sequelize = new Sequelize(process.env.DB_CONNECTION_STRING, {dialect: 'mysql'});
+
 const db = {};
-
-let sequelize;
-if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-    sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
-
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
